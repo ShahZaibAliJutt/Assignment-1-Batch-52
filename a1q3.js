4e3a7361d37793e9337534b0e225256cc9494213
Shah Zaib Ali Jutt <150579982+ShahZaibAliJutt@users.noreply.github.com>
2023-11-21 06:38:29 -1200

    Add files via upload

---
+++ a1q3.js
@@ -0,0 +1,5 @@
+"use strict";
+var name1 = "Shah Zaib Ali";
+var a1 = name1.toLowerCase();
+var a2 = name1.toUpperCase();
+console.log(a2);
---
+++ a1q3.ts
@@ -0,0 +1,4 @@
+var name1="Shah Zaib Ali";
+var a1=name1.toLowerCase();
+var a2=name1.toUpperCase();
+console.log(a2);
